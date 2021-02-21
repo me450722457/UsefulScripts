09a62a6aad3c842f92f4db414f0c19667e5f9e9b
GuJin <me450722457@vip.qq.com>
2021-02-21 22:49:43 +0800

    Remove queue in ping.py

--- ping.py
+++ ping.py
@@ -2,30 +2,37 @@ import threading
 import os
 import sys
 import time
-import queue
 from netaddr import IPNetwork, core
 from functools import wraps
 
-ip_queue = queue.Queue()
+ip_list = []
+
+
+class MyPing(threading.Thread):
+    def __init__(self, func, args, name=''):
+        super(MyPing, self).__init__()
+        self.name = name
+        self.func = func
+        self.args = args
+
+    def run(self):
+        self.func(*self.args)
 
 
 def get_ips(cidr):
-    # TODO: Need to fix when netmask not equal to 24
     try:
         ips = IPNetwork(cidr).iter_hosts()
         for ip in ips:
-            ip_queue.put(ip)
+            ip_list.append(ip.format())
     except core.AddrFormatError:
         print('Please enter the correct cidr or ip')
         sys.exit(1)
 
 
-def ping(ip_queue):
-    while not ip_queue.empty():
-        ip = ip_queue.get()
-        res = os.system('ping -c 1 %s 2>&1 >> /dev/null' % (ip))
-        if not res:
-            print('Success on %s' % ip)
+def ping(ip):
+    res = os.system('ping -c 1 %s 2>&1 >> /dev/null' % (ip))
+    if not res:
+        print('Success on %s' % ip)
 
 
 def exec_time(func):
@@ -44,8 +51,8 @@ def main():
     cidr = str(sys.argv[1])
     get_ips(cidr)
     thread_pool = []
-    for i in range(1, 255):
-        p = threading.Thread(target=ping, args=(ip_queue,))
+    for ip in ip_list:
+        p = MyPing(ping, (ip,))
         thread_pool.append(p)
     for i in thread_pool:
         i.start()
